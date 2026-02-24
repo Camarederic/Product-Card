e503a37d41194d19f0e7a747358efbff08dde5ad
Camarederic <camarederic@gmail.com>
2026-02-24 20:22:00 +0300

    added code to change images

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-'use strict'
+"use strict";
 
+const activeImage = document.querySelector(".active-image img");
+const images = document.querySelectorAll(".img");
+
+images.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    activeImage.src = item.firstElementChild.src;
+
+    images.forEach((img) => {
+      img.classList.remove("change");
+    });
+
+    item.classList.add("change");
+  });
+});
